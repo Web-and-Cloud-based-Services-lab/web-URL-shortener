afcfe0e843077fdc56b6bd56ff0ada9a1f2bc16e
Yudong-Fan <YF970919@hotmail.com>
2023-04-05 14:02:08 +0200

    attach initial dataset

--- src/dataInitiator.py
+++ src/dataInitiator.py
@@ -1,8 +1,12 @@
 from apiHandler import apiHandler
+import pandas as pd
+
+urls = pd.read_csv("../data/URLs.csv", names = ['url', 'class'])
 
 class DataInitiator:
     def __init__(self):   
-        self.urls = ['http://www.hilihili.com', 'http://www.vu.co.jp', 'http://www.helloworld.net'] 
+        # self.urls = ['http://www.hilihili.com', 'http://www.vu.co.jp', 'http://www.helloworld.net'] 
+        self.urls = urls['url'].tolist()[:10000]
 
     def sample_urls_to_database(self):
         apiHandler.create_many_urls(self.urls)
