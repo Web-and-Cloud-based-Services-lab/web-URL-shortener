efd50a8b7fa890f3a13bbfb7db7d30e4de49b1d8
Yuna C <yunabell@outlook.com>
2023-04-06 09:45:42 +0200

    update README

--- src/api.py
+++ src/api.py
@@ -1,5 +1,7 @@
+# This file defines APIs the server provides
 # Flask library is used to route APIs
-# Reference: https://flask.palletsprojects.com/en/2.2.x/quickstart/
+# The use of flask references the official documentation of flask:
+# https://flask.palletsprojects.com/en/2.2.x/quickstart/
 
 from flask import Flask
 from flask import request
@@ -8,7 +10,7 @@ from apiHandler import apiHandler
 
 
 app = Flask(__name__)
-cors = CORS(app)
+cors = CORS(app) # cors is added in advance to allow cors requests
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/', methods=["GET"])
@@ -58,7 +60,7 @@ def delete_url(url_id):
 
 @app.route('/<string:url_id>', methods=["PUT"])
 @cross_origin()
-def uodate_url(url_id):
+def update_url(url_id):
     url = apiHandler.get_url(url_id)
     if url == None:
         return "Error: URL id NOT FOUND", 404
