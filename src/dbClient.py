77e9a5c4a7e3c782cf7dd2e4a76245ad87ca8b1b
Yuna C <yunabell@outlook.com>
2023-04-22 17:08:40 +0200

    fix password

--- src/dbClient.py
+++ src/dbClient.py
@@ -3,6 +3,6 @@
 import pymongo 
 
 username = 'url_shortener'
-password = 'url_shortener_passwor'
+password = 'url_shortener_password'
 
 mongo_client = pymongo.MongoClient('mongodb://{user}:{pwd}@localhost:27017/admin'.format(user = username, pwd = password))
