5b26b529bb546276a90c5d190fcc9ac751778671
Yuna C <yunabell@outlook.com>
2023-05-08 12:40:16 +0200

    client for k8s

--- src/dbClient.py
+++ src/dbClient.py
@@ -1,8 +1,11 @@
 # settings of mongoDB client
 
 import pymongo 
+import os
 
 username = 'url_shortener'
 password = 'url_shortener_password'
 
-mongo_client = pymongo.MongoClient('mongodb://{user}:{pwd}@mongo:27017/admin'.format(user = username, pwd = password))
+# mongo_client = pymongo.MongoClient('mongodb://{user}:{pwd}@mongo:27017/admin'.format(user = username, pwd = password))
+mongo_client = pymongo.MongoClient('mongodb://{url}'.format(url=os.environ['DB_URL']))
+# mongo_client = pymongo.MongoClient('mongodb://{user}:{pwd}@{url}'.format(user = os.environ['USER_NAME'], pwd = os.environ['USER_PWD'], url = os.environ['DB_URL']))
